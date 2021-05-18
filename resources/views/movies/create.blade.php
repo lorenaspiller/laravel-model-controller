a12960b2699af96fec6428c0c91e75741d48c5be
Lorena Spiller <lorenaspiller@gmail.com>
2021-05-18 20:40:48 +0200

    added old() in forms

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -22,29 +22,29 @@
 
             <div class="form-group">
                 <label for="cover_image">Immagine</label>
-                <input type="text" class="form-control @error('cover_image') is-invalid @enderror" id="cover_image" name="cover_image" placeholder="URL Immagine">
+                <input type="text" class="form-control @error('cover_image') is-invalid @enderror" id="cover_image" name="cover_image" placeholder="URL Immagine" value="{{old('cover_image')}}">
             </div>
             <div class="form-group">
                 <label for="title">Titolo</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Titolo">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Titolo" value="{{old('title')}}">
             </div>
             <div class="form-group">
                 <label for="film_director">Regista</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="film_director" name="film_director" placeholder="Regista">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="film_director" name="film_director" placeholder="Regista" value="{{old('film_director')}}">
             </div>
             <div class="form-group">
                 <label for="genres">Generi</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="genres" name="genres" placeholder="Generi">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="genres" name="genres" placeholder="Generi" value="{{old('genres')}}">
             </div>
             <div class="form-group">
                 <label for="plot">Trama</label>
-                <textarea class="form-control @error('title') is-invalid @enderror" id="plot" name="plot" rows="8" placeholder="Trama..."></textarea>
+                <textarea class="form-control @error('title') is-invalid @enderror" id="plot" name="plot" rows="8" placeholder="Trama...">{{old('plot')}}</textarea>
             </div>
             <div class="form-group">
                 <label for="year">Anno</label>
                 <select class="form-control @error('title') is-invalid @enderror" id="year" name="year">
                     @for ($i = 1900; $i <= date("Y") + 1; $i++)
-                        <option value="{{$i}}">{{$i}}</option>
+                        <option value="{{$i}}" {{$i == old('year') ? 'selected' : ''}}>{{$i}}</option>
                     @endfor
                 </select>
             </div>
--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -25,29 +25,29 @@
                 <div class="edit-image-wrapper mb-4">
                     <img class="edit-form-img" src="{{$movie->cover_image}}" alt="{{$movie->cover_image}}">
                 </div>
-                <input type="text" class="form-control @error('cover_image') is-invalid @enderror" id="cover_image" name="cover_image" placeholder="URL Immagine" value="{{$movie->cover_image}}">
+                <input type="text" class="form-control @error('cover_image') is-invalid @enderror" id="cover_image" name="cover_image" placeholder="URL Immagine" value="{{old('cover_image') ? old('cover_image') : $movie->cover_image}}">
             </div>
             <div class="form-group">
                 <label for="title">Titolo</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Titolo" value="{{$movie->title}}">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Titolo" value="{{old('title') ? old('title') : $movie->title}}">
             </div>
             <div class="form-group">
                 <label for="film_director">Regista</label>
-                <input type="text" class="form-control @error('film_director') is-invalid @enderror" id="film_director" name="film_director" placeholder="Regista" value="{{$movie->film_director}}">
+                <input type="text" class="form-control @error('film_director') is-invalid @enderror" id="film_director" name="film_director" placeholder="Regista" value="{{old('film_director') ? old('film_director') : $movie->film_director}}">
             </div>
             <div class="form-group">
                 <label for="genres">Generi</label>
-                <input type="text" class="form-control @error('genres') is-invalid @enderror" id="genres" name="genres" placeholder="Generi" value="{{$movie->genres}}">
+                <input type="text" class="form-control @error('genres') is-invalid @enderror" id="genres" name="genres" placeholder="Generi" value="{{old('genres') ? old('genres') : $movie->genres}}">
             </div>
             <div class="form-group">
                 <label for="plot">Trama</label>
-                <textarea class="form-control @error('plot') is-invalid @enderror" id="plot" name="plot" rows="8" placeholder="Trama...">{{$movie->plot}}</textarea>
+                <textarea class="form-control @error('plot') is-invalid @enderror" id="plot" name="plot" rows="8" placeholder="Trama...">{{old('plot') ? old('plot') : $movie->plot}}</textarea>
             </div>
             <div class="form-group">
                 <label for="year">Anno</label>
                 <select class="form-control @error('title') is-invalid @enderror" id="year" name="year">
                     @for ($i = 1900; $i <= date("Y") + 1; $i++)
-                        <option value="{{$i}}" {{$i == $movie->year ? 'selected' : ''}}>{{$i}}</option>
+                        <option value="{{$i}}" {{old('year') == $i || $i == $movie->year ? 'selected' : ''}}>{{$i}}</option>
                     @endfor
                 </select>
             </div>
